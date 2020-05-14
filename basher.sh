2e4f9c5846b60b52004e16650a563c65a2fc33bb
Kushal <kushal-shrestha@hotmail.com>
2020-05-14 20:37:31 +0545

    Kiwiw this is the one

--- basher.sh
+++
@@ -1,9 +0,0 @@
-UNITY_VERSION=2019.2.5f1
-docker run -it --rm \
--e "UNITY_USERNAME=kushal-shrestha@hotmail.com" \
--e "UNITY_PASSWORD=Kushal9841508775" \
--e "TEST_PLATFORM=linux" \
--e "WORKDIR=/root/project" \
--v "$(pwd):/root/project" \
-gableroux/unity3d:$UNITY_VERSION \
-bash
